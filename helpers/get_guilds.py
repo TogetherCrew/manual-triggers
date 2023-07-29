c56874c3f236b5de1d265e9fef69190be9d87163
Cyrille Derche <c.derche@me.com>
2023-07-29 12:31:37 +0200

    update get_guilds

--- helpers/get_guilds.py
+++ helpers/get_guilds.py
@@ -1,6 +1,4 @@
-from mongodb_connection import client
-
-def get_guilds(db=client.get_default_database()):
+def get_guilds(db):
     collection = db["guilds"]
     guilds = collection.find({}, {"name": 1, "guildId": 1})
     return guilds
