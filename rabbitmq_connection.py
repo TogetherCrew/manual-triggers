2bbaba368eb8632969285b4d5cfa7830095b2cb3
Cyrille <1618640+cyri113@users.noreply.github.com>
2023-08-02 11:47:15 +0200

    Update rabbitmq_connection.py

--- rabbitmq_connection.py
+++ rabbitmq_connection.py
@@ -5,12 +5,11 @@ from tc_messageBroker import RabbitMQ
 
 load_dotenv()
 
-if __name__ == "__main__":
-    broker_url = os.getenv("RABBIT_HOST")
-    port = os.getenv("RABBIT_PORT")
-    username = os.getenv("RABBIT_USER")
-    password = os.getenv("RABBIT_PASSWORD")
+broker_url = os.getenv("RABBIT_HOST")
+port = os.getenv("RABBIT_PORT")
+username = os.getenv("RABBIT_USER")
+password = os.getenv("RABBIT_PASSWORD")
 
-    rabbit_mq = RabbitMQ(
-        broker_url=broker_url, port=port, username=username, password=password
-    )
+rabbit_mq = RabbitMQ(
+    broker_url=broker_url, port=port, username=username, password=password
+)
